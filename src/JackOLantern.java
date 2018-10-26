c124f3f5452c5af4040448656bc0e9edc78588e6
BT_1N3_23 <jhan3537@bths.edu>
2018-10-26 11:33:17 -0400

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,7 @@
+public class JackOLantern extends java.lang.Object {
+    String[][] faceFeatures;
+
+    public JackOLantern(java.lang.String[][] faceFeatures) {
+        this.faceFeatures = faceFeatures;
+    }
+}
